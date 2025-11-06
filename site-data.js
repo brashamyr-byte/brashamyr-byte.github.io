9e0732e5f60d17886775b0d19ea2f573d6038960
DEXTT <brashamyr@gmail.com>
2025-11-06 21:17:29 +0300

    Delete site-data.js

--- site-data.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.search-form').addEventListener('submit', e => {
-  e.preventDefault();
-  const query = e.target.querySelector('input').value.toLowerCase();
-  const cards = document.querySelectorAll('.card');
-  cards.forEach(card => {
-    const title = card.querySelector('h3').textContent.toLowerCase();
-    card.style.display = title.includes(query) ? 'block' : 'none';
-  });
-});
